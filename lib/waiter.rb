62456bb900e7ebe2e3a2c7b8ba9ff6242bcde850
Billy Ferrara <willfira717@gmail.com>
2020-06-07 18:53:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -3,9 +3,16 @@ class Waiter
   attr_reader :name
   attr_accessor :experience
 
+  @@all = []
+
   def initialize(name, experience)
     @name = name
     @experience = experience
+    @@all << self 
+  end
+
+  def self.all
+    @@all
   end
 
 end

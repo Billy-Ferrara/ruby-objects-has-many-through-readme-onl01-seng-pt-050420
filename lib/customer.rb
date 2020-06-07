79a9d17c12b3c1529bb70bab0d61a93f04a104f9
Billy Ferrara <willfira717@gmail.com>
2020-06-07 18:47:40 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,14 @@
 class Customer
 
     attr_reader :name
-    attr_accessor :age 
+    attr_accessor :age
+
+    @@all = []
 
   def initialize(name, age)
     @name = name
     @age = age
+    @@all << self 
   end
 
 end

32dc3965734b655402b6e47e43221e451bbbced0
Billy Ferrara <willfira717@gmail.com>
2020-06-07 18:55:58 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,12 +1,12 @@
 class Meal
 
   attr_reader :customer
-  attr_accessor :waiter, :total, :tip 
+  attr_accessor :waiter, :total, :tip
 
   def initialize(waiter, customer, total, tip)
     @waiter = waiter
     @customer = customer
-    @total = tip
+    @total = total
     @tip = tip
   end
 
